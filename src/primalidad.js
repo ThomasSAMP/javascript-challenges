6efe8a39d7d9460854eefd2b21fc289d0537dec6
Thomas <58904022+ThomasSAMP@users.noreply.github.com>
2022-09-16 11:30:32 +0200

    primalidad - ok

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
-  // your code here
+  if(number === 1 || !Number.isInteger(number) || number <= 0) return false
+  
+  for(var i = 2; i < number; i++) {
+    if(number%i === 0) return false;
+  }
+  return true;
 }
 
 module.exports = trialDivision;
